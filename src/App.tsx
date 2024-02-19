54e907df58fb601dfb4d8d1bbab905dd7fe7e767
Madison <malab@shaw.ca>
2024-02-19 00:11:59 -0600

    made some pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,22 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { SignIn } from './pages/SignIn';
+import { Home } from './pages/Home';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <Routes>   
+            <Route path='/' element={<SignIn />} />
+            <Route path='/home' element={<Home />} />
+          </Routes>
+        </header>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/pages/Home.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export const Home = () => {
+
+  return (
+    <>
+      <p>Hello, we are now on the home page</p>
+    </>
+  )
+}
