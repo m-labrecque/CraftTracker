7bf7ad82c16df5b7ec95eaf0fb2dd84303487622
Madison <malab@shaw.ca>
2024-02-20 18:39:28 -0600

    created popup for creating a new project (doesnt actually do anything yet)

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -1,4 +1,4 @@
-import { Box, Button, Grid, Paper, ThemeProvider, Typography } from "@mui/material"
+import { Box, Button, Dialog, DialogContent, Grid, Paper, TextField, ThemeProvider, Typography } from "@mui/material"
 import React from "react"
 import { Project } from "../AppBaseTypes";
 import { mainTheme } from "../themes";
@@ -8,6 +8,8 @@ import { useNavigate } from "react-router-dom";
 export const Projects = () => {
   const [projects, setProjects] = React.useState<Project[]>([]);
   const navigate = useNavigate();
+  const [open, setOpen] = React.useState<boolean>(false);
+  const [newProjectName, setNewProjectName] = React.useState<string>("");
 
   React.useEffect(() => {
     setProjects([{Name: "Folklore Cartigan"}, {Name: "Patchwork Cartigan"}]);
@@ -16,6 +18,25 @@ export const Projects = () => {
   function gotoProject(name: string) {
     console.log(name);
     navigate('/projecthome', {state: {ProjectName: name}});
+  }
+
+  const handleOpen = () => {
+    console.log("open");
+    setOpen(true);
+  }
+
+  const handleClose = () => {
+    console.log("close");
+    setOpen(false);
+  }
+
+  const handleCreate = () => {
+    console.log("create");
+    setOpen(false);
+  }
+
+  const handleNameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setNewProjectName(event.target.value);
   }
   
   return (
@@ -41,6 +62,18 @@ export const Projects = () => {
         ))}
       </Grid>
       </Box>
+      <Button onClick={handleOpen}>New Project</Button>
+
+      {/* popup for creating new project */}
+      <Dialog open={open} onClose={handleClose}>
+        <DialogContent>
+          <TextField 
+            id="Project Name"
+            onChange={handleNameChange}/>
+          <Button onClick={handleCreate}>Create</Button>
+          <Button onClick={handleClose}>Cancel</Button>
+        </DialogContent>
+      </Dialog>
     </ThemeProvider>
   )
 }
