64f7a9ae631e6d6bac2224fad6329605613d55ab
Madison <malab@shaw.ca>
2024-02-20 23:27:23 -0600

    project gets loaded from database when project home page is loaded

--- src/pages/ProjectHome.tsx
+++ src/pages/ProjectHome.tsx
@@ -4,14 +4,35 @@ import { mainTheme } from "../themes"
 import { Header } from "../headers/Header"
 import { Grid, Paper, Typography } from "@mui/material"
 import { useLocation } from "react-router-dom"
+import { Project } from "../AppBaseTypes"
+import { doc, getDoc } from "firebase/firestore"
+import { db } from "../FireBase"
+import { getAuth } from "firebase/auth"
 
 
 export const ProjectHome = () => {
   const location = useLocation();
+  const auth = getAuth(); 
+
   const [name, setName] = React.useState<string>("");
+  const [currentProject, setCurrentProject] = React.useState<Project>();
+  const projectName = location.state?.ProjectName || "";
+
+  const getProject = async() => {
+    const userRef = doc(db, 'users', auth.currentUser?.uid || "");
+    const userSnap = await getDoc(userRef);
+    const data = userSnap.data();
+
+    if (data?.Projects) {
+      const filteredProjects = data.Projects.filter((n: {Name: string}) => n.Name === projectName);
+      setCurrentProject(filteredProjects[0]);
+      console.log(currentProject);
+    }
+  }
 
   React.useEffect(() => {
     setName(location.state?.ProjectName);
+    getProject();
   }, [])
 
   return (
