ed693f7c71326f50a523bc49dae419c72f1ded3b
Madison <malab@shaw.ca>
2024-05-08 15:26:25 -0600

    added logout button to header

--- src/headers/Header.tsx
+++ src/headers/Header.tsx
@@ -1,9 +1,22 @@
-import { AppBar, Button, IconButton, ThemeProvider, Toolbar, Typography } from "@mui/material"
+import { AppBar, Button, ThemeProvider, Toolbar } from "@mui/material"
 import React from "react"
 import { mainTheme } from "../themes"
-import { Link } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom"
+import { getAuth, signOut } from "firebase/auth"
 
 export const Header = () => {
+  const auth = getAuth();
+  const navigate = useNavigate();
+  
+  const logOut = () => {
+    signOut(auth).then(() => {
+      console.log("logged out successfully");
+      navigate("/");
+    }).catch((e) => {
+      console.log("log out failed" + e)
+    })
+  }
+
   return (
     <ThemeProvider theme={mainTheme}>
       <AppBar position="fixed">
@@ -12,6 +25,7 @@ export const Header = () => {
             <Button sx={{backgroundColor: "#ffffff"}}>Home</Button>
           </Link>
           {/* <Typography style={{color: '#AAADBC'}}>Header</Typography> */}
+          <Button onClick={logOut} sx={{backgroundColor: "#ffffff"}}>Sign Out</Button>
         </Toolbar>
         
       </AppBar>
